a8a1929b28be65cbb859ec03ecf3a50eca334f07
lwairore <kwairore@gmail.com>
2020-01-31 16:57:45 +0300

    create fetchAllServices function that fetches all services from the server.

---
+++ src/app/services/get-all-services.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { environment } from '../../environments/environment';
+import {shareReplay} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetAllServicesService {
+  httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  fetchAllServices():Observable<any>{
+    return this.http.get(environment.allServices, {headers: this.httpHeaders} ).pipe(
+shareReplay()
+    )
+  }
+}
