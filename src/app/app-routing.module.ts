44cc88d8c29a4ce9bb353331d2c58e25a5b56e62
lwairore <kwairore@gmail.com>
2020-01-31 17:23:44 +0300

    create "appointment" path that is mapped to AppointmentComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from "./homepage/homepage.component";
+// import { TestTemplatesComponent } from './test-templates/test-templates.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomepageComponent },
+  // { path: "test-component", component: TestTemplatesComponent },
+  { path: "appointment", component: AppointmentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
