697ce4f79ed22c6dd17736c0eb4c265f8bbfb038
lwairore <kwairore@gmail.com>
2020-01-31 18:04:37 +0300

    initialized allNavBarItems as an empty array.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {GetAllNavbarItemsService} from '../services/get-all-navbar-items.service';
-import {GetAllServicesService} from '../services/get-all-services.service';
-import {GetAllDoctorsService} from '../services/get-all-doctors.service';
+import { GetAllNavbarItemsService } from '../services/get-all-navbar-items.service';
+import { GetAllServicesService } from '../services/get-all-services.service';
+import { GetAllDoctorsService } from '../services/get-all-doctors.service';
 import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-homepage',
@@ -14,67 +14,71 @@ import { environment } from '../../environments/environment';
   ]
 })
 export class HomepageComponent implements OnInit {
-  allNavBarItems:[];
-  allServices:[];
+  allNavBarItems = [];
+  allServices: [];
   batchedServices = [];
-  allDoctors : [];
+  allDoctors: [];
   mediaBaseURL: string = `${environment.mediaBaseUrl}`;
 
   constructor(
-    private getAllNavBarItemsService:GetAllNavbarItemsService,
-    private getAllServicesService:GetAllServicesService,
-    private getAllDoctorsService:GetAllDoctorsService
+    private getAllNavBarItemsService: GetAllNavbarItemsService,
+    private getAllServicesService: GetAllServicesService,
+    private getAllDoctorsService: GetAllDoctorsService
   ) { }
 
   ngOnInit() {
-this.getAllNavBarItems();
-this.getAllServices();
-this.getAllDoctors()
+    this.getAllNavBarItems();
+    this.getAllServices();
+    this.getAllDoctors()
 
 
 
   }
 
-  getAllNavBarItems(){
+  getAllNavBarItems() {
     this.getAllNavBarItemsService.fetchAllNavbarItems().subscribe(
-      navBarItemsData => {
-        this.allNavBarItems = navBarItemsData;
-        
-      }
+      (res) => {
+        console.log(res);
+        switch (res['status']) {
+          case 200:
+            this.allNavBarItems = res['all_navbar_items'];
+            break;
+        }
+      }, (err) => { console.log(err) }
     )
   }
 
-  getAllServices(){
+  getAllServices() {
     this.getAllServicesService.fetchAllServices().subscribe(
       servicesData => {
         this.allServices = servicesData;
-        console.log(servicesData);
+        // console.log(servicesData);
         let numberOfRows = 2;
         let numberOfItemsInEachRow = parseInt((this.allServices.length / numberOfRows).toFixed());
-        for(var i=0; i<2; i++){
-          if (this.allServices.length >= numberOfItemsInEachRow){
+        for (var i = 0; i < 2; i++) {
+          if (this.allServices.length >= numberOfItemsInEachRow) {
             this.batchedServices.push(this.allServices.splice(0, numberOfItemsInEachRow))
           }
-          if (this.allServices.length < 3 && this.allServices.length > 0 ){
+          if (this.allServices.length < 3 && this.allServices.length > 0) {
             this.batchedServices.push(this.allServices);
             this.allServices = [];
           }
 
         }
 
-        console.log(this.batchedServices)
+        // console.log(this.batchedServices)
       }
     )
   }
 
-  getAllDoctors(){
+  getAllDoctors() {
     this.getAllDoctorsService.fetchAllDoctors().subscribe(
       doctorsData => {
         console.log(doctorsData)
         console.log(doctorsData['data'])
         this.allDoctors = doctorsData['data'];
-        console.log(doctorsData);
-        
+        // console.log(doctorsData);
+
       }
     )
   }
